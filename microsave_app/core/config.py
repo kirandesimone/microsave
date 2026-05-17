d93b1169995825a50746bb99eb9b9f88914b3d57
joshjyu <jae@joshyu.com>
2026-05-16 20:43:26 -0700

    Fix overwriting created_at timestamp
    Fixed overwriting created_at timestamp every time an existing save is updated by
    separating created_at using setOnInsert. Also fixed a critical typo in config.py.

--- microsave_app/core/config.py
+++ microsave_app/core/config.py
@@ -6,12 +6,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 
+
 class Settings(BaseSettings):
     """Runtime configuration for the Save Service"""
+
     model_config = SettingsConfigDict(
-            env_file=BASE_DIR / ".env.mongodb", 
-            env_file_encoding="utf-8",
-            extra="ignore"
+        env_file=BASE_DIR / ".env.mongodb",
+        env_file_encoding="utf-8",
+        extra="ignore",
     )
 
     app_name: str = "Save Service"
@@ -21,7 +23,8 @@ class Settings(BaseSettings):
     port: int = 8000
 
     mongodb_uri: str = Field(validation_alias="MONGODB_URI")
-    mongodb_db_name: str = Field(validaion_alias="MONGODB_DB_NAME")
+    mongodb_db_name: str = Field(validation_alias="MONGODB_DB_NAME")
+
 
 def get_settings() -> Settings:
     """Cached settings accessor."""
--- microsave_app/services/mongo.py
+++ microsave_app/services/mongo.py
@@ -5,6 +5,7 @@ from microsave_app.models.save import SaveDocument
 
 async def upsert_save(sd: SaveDocument, db: AsyncDatabase):
     doc = sd.model_dump()
+    created_at = doc.pop("created_at")
 
     await db["saves"].update_one(
         {
@@ -12,12 +13,18 @@ async def upsert_save(sd: SaveDocument, db: AsyncDatabase):
             "user_id": sd.user_id,
             "save_slot": sd.save_slot,
         },
-        {"$set": doc},
+        {
+            "$set": doc,
+            # Only set created_at if it's a new record
+            "$setOnInsert": {"created_at": created_at},
+        },
         upsert=True,
     )
 
 
-async def get_save(client_app_id: str, user_id: str, save_slot:str, db: AsyncDatabase):
+async def get_save(
+    client_app_id: str, user_id: str, save_slot: str, db: AsyncDatabase
+):
     return await db["saves"].find_one(
         {
             "client_app_id": client_app_id,
